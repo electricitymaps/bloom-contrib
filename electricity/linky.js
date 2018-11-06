82ea4037c1119fe0f2b208445b1feef05a6ecc45
Olivier Corradi <olivier.corradi@gmail.com>
2018-11-06 16:40:18 +0100

    Add linky and zoe

---
+++ electricity/linky.js
@@ -0,0 +1,152 @@
+import moment from 'moment';
+import request from 'superagent';
+
+import { ACTIVITY_TYPE_ELECTRICITY } from '../../constants';
+
+const GRANULARITY = {
+  day: 'urlCdcJour',
+  hour: 'urlCdcHeure',
+};
+const DURATION_HOURS = {
+  day: 24,
+  hour: 1,
+};
+const PPID = 'lincspartdisplaycdc_WAR_lincspartcdcportlet';
+
+// Create an agent that can hold cookies
+const agent = request.agent();
+
+/*
+  Doc: https://github.com/PhilBri/Node-Linky/blob/master/linky.js
+  Note we can't use the same implementation as RN `fetch` uses
+  XMLHttpRequest which always follows redirects, thus preventing
+  us from grabbing cookies. Else we would be able to set
+  .redirects(0) on `agent`.
+*/
+
+async function logIn(username, password) {
+  const res = await agent
+    .post('https://espace-client-connexion.enedis.fr/auth/UI/Login')
+    .type('form')
+    .send({
+      IDToken1: username,
+      IDToken2: password,
+      SunQueryParamsString: 'cmVhbG09cGFydGljdWxpZXJz',
+    });
+
+  if (!res.ok) {
+    console.error(res);
+    throw Error('Error while logging in.');
+  }
+  if (res.xhr.responseURL.indexOf('messages') !== -1) {
+    // Redirected to https://espace-client-connexion.enedis.fr/messages/{information,inexistant}.html
+    // which indicates a login error.
+    throw Error('Invalid credentials');
+  }
+}
+
+export async function connect(requestLogin, requestWebView) {
+  // Here we can request credentials etc..
+
+  // Here we can use two functions to invoke screens
+  // requestLogin() or requestWebView()
+  const { username, password } = await requestLogin();
+  await logIn(username, password);
+
+  // Set state to be persisted
+  return {
+    username,
+    password,
+  };
+}
+
+export function disconnect() {
+  // Here we should do any cleanup (deleting tokens etc..)
+  return {};
+}
+
+export async function collect(state) {
+  const { username, password } = state;
+  // LogIn to set Cookies
+  await logIn(username, password);
+
+  // For now we're gathering daily data
+  const frequency = 'day';
+
+  // By default, go back 1 month
+  // (we can't go back further using a single API call)
+  const startDate = state.lastFullyCollectedDay || moment().subtract(1, 'month').format('DD/MM/YYYY');
+  const endDate = moment().format('DD/MM/YYYY');
+
+  const query = {
+    p_p_col_pos: 1,
+    p_p_lifecycle: 2,
+    p_p_col_count: 2,
+    p_p_state: 'normal',
+    p_p_mode: 'view',
+    p_p_cacheability: 'cacheLevelPage',
+    p_p_col_id: 'column-1',
+    p_p_id: PPID,
+    p_p_resource_id: GRANULARITY[frequency],
+  };
+
+  const payload = {};
+  payload[`_${PPID}_dateDebut`] = startDate;
+  payload[`_${PPID}_dateFin`] = endDate;
+
+  const res = await agent
+    .withCredentials()
+    .post('https://espace-client-particuliers.enedis.fr/group/espace-particuliers/suivi-de-consommation')
+    .query(query)
+    .type('form')
+    .send(payload);
+
+  const json = JSON.parse(res.text);
+  if (json.etat.valeur === 'erreur') {
+    throw Error('Error while fetching data.');
+  }
+
+  const { data, periode } = json.graphe;
+
+  // TODO: Double check timezones?
+  const startMoment = moment(periode.dateDebut, 'DD/MM/YYYY');
+  const endMoment = moment(periode.dateDebut, 'DD/MM/YYYY');
+
+  /*
+    Schema:
+    data = [
+      {"ordre": 1066, "valeur": 2.206}
+      ...
+    ]
+    `ordre` is an index
+    `valeur` is in kWh
+    Negative `valeur` means data is unknown
+  */
+
+  const activities = data.map((d, i) => {
+    // Remember that moment methods are mutable,
+    // which means we should make a copy by wrapping in `moment()``
+    const dateMoment = moment(startMoment).add(i, frequency);
+    return {
+      // Create a unique id to make sure data will be overwritten
+      id: `linky${dateMoment.toISOString()}`,
+      datetime: dateMoment.toDate(),
+      activityType: ACTIVITY_TYPE_ELECTRICITY,
+      energyWattHours: d.valeur >= 0 ? (d.valeur * 1000.0) : null,
+      durationHours: DURATION_HOURS[frequency],
+    };
+  }).filter(d => d.energyWattHours); // we currently ignore invalid hours.
+
+  // Subtract one day to make sure we always have a full day
+  const lastFullyCollectedDay = endMoment.subtract(1, 'day').format('DD/MM/YYYY');
+
+  return { activities, lastFullyCollectedDay };
+}
+
+export const config = {
+  label: 'Linky',
+  type: 'electricity',
+  subType: 'smartMeter',
+  isPrivate: true,
+  // minRefreshInterval: 60
+};
---
+++ electricity/renault-zoe.js
@@ -0,0 +1,138 @@
+import moment from 'moment';
+import request from 'superagent';
+
+import { ACTIVITY_TYPE_ELECTRIC_VEHICLE_CHARGING } from '../../constants';
+
+const EXPIRED_TOKEN_MESSAGE = 'com.worldline.renault.myzeonline.exception.InvalidAuthenticationException.ExpiredToken';
+const BATTERY_SIZE = 22000; // in Wh
+
+/*
+  Doc: https://shkspr.mobi/blog/2016/10/reverse-engineering-the-renault-zoe-api/
+*/
+
+async function _fetchData(vin, bearerToken, startMonth, endMonth) {
+  const url = `https://www.services.renault-ze.com/api/vehicle/${vin}/charge/history`;
+  return request
+    .get(url)
+    .query({
+      begin: startMonth,
+      end: endMonth,
+    })
+    .set('Authorization', `Bearer ${bearerToken}`)
+    .set('Accept', 'application/json');
+}
+
+async function _refreshToken(state) {
+  const { token, refreshToken } = state;
+  const res = await request
+    .post('https://www.services.renault-ze.com/api/user/token/refresh')
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send({ token, refresh_oken: refreshToken });
+  return { token: res.body.token };
+}
+
+export async function connect(requestLogin, requestWebView) {
+  // Here we can request credentials etc..
+
+  // Here we can use two functions to invoke screens
+  // requestLogin() or requestWebView()
+  const { username, password } = await requestLogin();
+  const res = await request
+    .post('https://www.services.renault-ze.com/api/user/login')
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send({ username, password });
+
+  if (res.body.user.associated_vehicles.length > 1) {
+    throw Error('More than one VIN number detected.');
+  }
+
+  // Set state to be persisted
+  return {
+    token: res.body.token,
+    refreshToken: res.body.refresh_token,
+    vin: res.body.user.associated_vehicles[0].VIN,
+  };
+}
+
+export function disconnect() {
+  // Here we should do any cleanup (deleting tokens etc..)
+  return {};
+}
+
+export async function collect(state) {
+  const { vin } = state;
+  const startMonth = state.lastFullyCollectedMonth || moment().subtract(2, 'year').format('MMYY');
+  const endMonth = moment().format('MMYY');
+  // Subtract one month to make sure we always have a full month
+  const lastFullyCollectedMonth = moment().subtract(1, 'month').format('MMYY');
+
+  // TODO: Describe conditions under which `connect` should be called upon failure
+  const newState = {
+    ...state,
+    lastFullyCollectedMonth,
+  };
+  let res;
+  try {
+    res = await _fetchData(vin, state.token, startMonth, endMonth);
+  } catch (e) {
+    res = e.response;
+    if (res && res.status === 401 && JSON.parse(res.text).message === EXPIRED_TOKEN_MESSAGE) {
+      const { token } = await _refreshToken(state);
+      newState.token = token;
+      res = await _fetchData(vin, token, startMonth, endMonth);
+    } else {
+      // Throw
+      throw new Error(e);
+    }
+  }
+
+  if (res.status === 204) {
+    // No content, `res.body` will be null
+    return { activities: [], state: newState };
+  }
+
+  /* Look at charge events that are between `type = "START_NOTIFICATION"`
+     and `type = "END_NOTIFICATION"`, and interpolate charging power
+     using given battery size and measured `charge_level`
+  */
+  const activities = [];
+  res.body.reverse().forEach((d, i) => {
+    if (i === 0) { return; }
+    const prev = res.body[i - 1];
+    if (d.type === 'END_NOTIFICATION' && prev.type === 'START_NOTIFICATION') {
+      // Interpolate between start and end
+      const start = moment(prev.date);
+      const end = moment(d.date);
+      const duration = end.valueOf() - start.valueOf();
+      const totalPower = (d.charge_level - prev.charge_level) / 100.0 * BATTERY_SIZE;
+      if (totalPower <= 0) { return; }
+      for (
+        let now = moment(start).startOf('hour');
+        // now.isSameOrBefore(moment(end).endOf('hour'));
+        now.valueOf() <= moment(end).endOf('hour').valueOf();
+        now.add(1, 'hour'))
+      {
+        activities.push({
+          activityType: ACTIVITY_TYPE_ELECTRIC_VEHICLE_CHARGING,
+          datetime: now.toDate(),
+          durationHours: 1,
+          energyWattHours: 1.0 / duration * totalPower * (
+            Math.min(end.valueOf(), moment(now).add(1, 'hour').valueOf())
+            - Math.max(start.valueOf(), now.valueOf())),
+        });
+      }
+    }
+  });
+
+  return { activities, state: newState };
+}
+
+export const config = {
+  label: 'Renault Zoé',
+  type: 'electricity',
+  subType: 'electricVehicle',
+  isPrivate: true,
+  // minRefreshInterval: 60
+};
