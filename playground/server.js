83ef8f8f1742bcca9530220db133a5a8214c037b
Boris Kubiak <boris.kubiak@p3-group.com>
2019-07-15 15:12:49 +0200

    Create env file if it doesn't exist

--- playground/server.js
+++ playground/server.js
@@ -1,5 +1,10 @@
 // Transpile all code following this line with babel and use 'env' (aka ES6) preset.
 require('babel-register')();
 
+// Create empty env file if it doesn't exist
+const fs = require('fs');
+if (!fs.existsSync('./integrations/env.json')) {
+  fs.writeFileSync('./integrations/env.json', '{}');
+}
 // Import the rest of our application.
 module.exports = require('./src/index.js');
