9260cafda9609847c739e34434f3233349a239f8
lauvrenn <patiw0007@gmail.com>
2019-08-05 22:42:20 +0200

    Implements requested changes

--- integrations/transportation/ryanair.js
+++ integrations/transportation/ryanair.js
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import request from 'superagent';
 import { ACTIVITY_TYPE_TRANSPORTATION, TRANSPORTATION_MODE_PLANE } from '../../definitions';
+import { HTTPError } from '../utils/errors';
 
 const agent = request.agent();
 const BASE_URL = 'https://api.ryanair.com/userprofile/rest/api/v1/';
@@ -8,7 +9,7 @@ const LOGIN_URL = `${BASE_URL}login`;
 const PROFILE_URL = `${BASE_URL}secure/users/`;
 // user info available at `${PROFILE_URL}${customerId}/profile/full/`
 
-async function logIn(username, password, logger) {
+async function logIn(username, password) {
   const res = await agent
     .post(LOGIN_URL)
     .type('application/x-www-form-urlencoded')
@@ -18,8 +19,7 @@ async function logIn(username, password, logger) {
     });
 
   if (!res.ok) {
-    logger.logDebug('Error while logging in.');
-    throw Error('We\'re not logged in.');
+    throw HTTPError('Error while logging in.');
   }
 
   return {
@@ -28,7 +28,7 @@ async function logIn(username, password, logger) {
   };
 }
 
-async function connect(requestLogin, requestWebView, logger) {
+async function connect(requestLogin, requestWebView) {
   // Here we can request credentials etc..
 
   // Here we can use two functions to invoke screens
@@ -36,10 +36,10 @@ async function connect(requestLogin, requestWebView, logger) {
   const { username, password } = await requestLogin();
 
   if (!(password || '').length) {
-    throw Error('Password cannot be empty');
+    throw HTTPError('Password cannot be empty');
   }
 
-  return logIn(username, password, logger);
+  return logIn(username, password);
 }
 
 function disconnect() {
@@ -49,74 +49,77 @@ function disconnect() {
 
 async function getAllFlights(bookings, customerId, token) {
   const allFlights = [];
-
-  await Promise.all(bookings.map(async (entry) => {
-    await agent
-      .put(`${PROFILE_URL}${customerId}/bookings/booking`)
-      .set('Accept', 'application/json')
-      .set('X-Auth-Token', token)
-      .send({
-        bookingId: entry.bookingId,
-        pnr: entry.pnr,
-      })
-      .then((res) => {
-        if (res.ok) {
-          res.body.Flights.forEach((singleFlight) => {
-            allFlights.push({
-              bookingId: res.body.BookingId,
-              flightInfo: singleFlight,
-              pnr: res.body.RecordLocator,
+  try {
+    await Promise.all(bookings.map(async (entry) => {
+      await agent
+        .put(`${PROFILE_URL}${customerId}/bookings/booking`)
+        .set('Accept', 'application/json')
+        .set('X-Auth-Token', token)
+        .send({
+          bookingId: entry.bookingId,
+          pnr: entry.pnr,
+        })
+        .then((res) => {
+          if (res.ok) {
+            res.body.Flights.forEach((singleFlight) => {
+              allFlights.push({
+                bookingId: res.body.BookingId,
+                flightInfo: singleFlight,
+                pnr: res.body.RecordLocator,
+              });
             });
-          });
-        }
-      });
-  }));
+          }
+        });
+    }));
+  } catch (error) {
+    throw HTTPError(`Error while fetching past flights. ${error}`);
+  }
+
   return allFlights;
 }
 
-async function getPastBookings(customerId, token, logger) {
+async function getPastBookings(customerId, token) {
   const pastBookings = await agent
     .get(`${PROFILE_URL}${customerId}/bookings/past`)
     .set('Accept', 'application/json')
     .set('X-Auth-Token', token);
 
   if (!pastBookings.ok) {
-    logger.debug(`Error while fetching past bookings. ${pastBookings}`);
-    throw Error('We couldn\'t find any past bookings.');
+    throw HTTPError(`Error while fetching past bookings. ${pastBookings}`);
   }
 
   return pastBookings.body.bookings.filter(entry => entry.status === 'Confirmed');
 }
 
-async function getActivities(pastBookings, customerId, token, logger) {
+async function getActivities(pastBookings, customerId, token) {
   // WHY: there can be multiple flights in one booking
-  const activities = await getAllFlights(pastBookings, customerId, token)
-    .then((allFlights) => {
-      // WHY: there can be multiple bookings for the same flight (e.g. for different passengers)
-      // HOW: filters flights with the same flight number
-      const uniqueFlights = Array.from(new Set(allFlights.map(a => a.flightInfo.FlightNumber)))
-        .map(mappedFlightNumber => allFlights.find(a => a.flightInfo.FlightNumber === mappedFlightNumber));
-      return Object.values(uniqueFlights)
-        .map(k => ({
-          // TODO: make sure it's always unique
-          id: `B${k.bookingId}${k.flightInfo.FlightNumber}PNR${k.pnr}`,
-          datetime: k.flightInfo.DepartLocal,
-          durationHours: moment(k.flightInfo.Arrive).diff(moment(k.flightInfo.Depart), 'minutes') / 60,
-          activityType: ACTIVITY_TYPE_TRANSPORTATION,
-          transportationMode: TRANSPORTATION_MODE_PLANE,
-          carrier: 'Ryanair',
-          departureAirportCode: k.flightInfo.Origin,
-          destinationAirportCode: k.flightInfo.Destination,
-        }));
-    });
+  const allFlights = await getAllFlights(pastBookings, customerId, token);
+
+  // WHY: there can be multiple bookings for the same flight (e.g. for different passengers)
+  // HOW: filters flights with the same flight number
+  const uniqueFlights = Array.from(new Set(allFlights.map(a => a.flightInfo.FlightNumber)))
+    .map(mappedFlightNumber => allFlights.find(a => a.flightInfo.FlightNumber === mappedFlightNumber));
+
+  const activities = Object.values(uniqueFlights)
+    .map(k => ({
+      // TODO: make sure it's always unique
+      id: `B${k.bookingId}${k.flightInfo.FlightNumber}PNR${k.pnr}`,
+      datetime: k.flightInfo.DepartLocal,
+      durationHours: moment(k.flightInfo.Arrive).diff(moment(k.flightInfo.Depart), 'minutes') / 60,
+      activityType: ACTIVITY_TYPE_TRANSPORTATION,
+      transportationMode: TRANSPORTATION_MODE_PLANE,
+      carrier: 'Ryanair',
+      departureAirportCode: k.flightInfo.Origin,
+      destinationAirportCode: k.flightInfo.Destination,
+    }));
 
   return activities;
 }
 
-async function collect(state, logger) {
+async function collect(state) {
   const { token, customerId } = state;
-  const pastBookings = await getPastBookings(customerId, token, logger);
-  const activities = await getActivities(pastBookings, customerId, token, logger);
+  const pastBookings = await getPastBookings(customerId, token);
+  const activities = await getActivities(pastBookings, customerId, token);
 
   return { activities, state };
 }
