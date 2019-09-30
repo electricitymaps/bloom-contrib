923a833e3cd64ccf54725faef0cd8d5323cc4fc0
Olivier Corradi <olivier.corradi@gmail.com>
2019-09-30 11:35:32 +0200

    Linky supports hourly data

--- integrations/electricity/linky.js
+++ integrations/electricity/linky.js
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import request from 'superagent';
+import groupBy from 'lodash/groupBy';
 
 import { ACTIVITY_TYPE_ELECTRICITY } from '../../definitions';
 import { AuthenticationError, HTTPError } from '../utils/errors';
@@ -11,6 +12,10 @@ const GRANULARITY = {
 const DURATION_HOURS = {
   day: 24,
   hour: 1,
+};
+const STEP_GRANULARITY = {
+  day: 1,
+  hour: 0.5, // half-hourly data is given
 };
 const PPID = 'lincspartdisplaycdc_WAR_lincspartcdcportlet';
 
@@ -25,6 +30,16 @@ const agent = request.agent();
   .redirects(0) on `agent`.
 */
 
+function getResponseURL(res) {
+  if (res.redirects) {
+    return (res.redirects || [])[0];
+  }
+  if (res.xhr) {
+    return res.xhr.responseURL;
+  }
+  return null;
+}
+
 async function logIn(username, password) {
   const res = await agent
     .post('https://espace-client-connexion.enedis.fr/auth/UI/Login')
@@ -32,18 +47,35 @@ async function logIn(username, password) {
     .send({
       IDToken1: username,
       IDToken2: password,
+      // <--- adding this redirect seems to allow us to check that the login
+      // works, else it might just redirect to an empty page..
+      // goto: 'aHR0cHM6Ly9lc3BhY2UtY2xpZW50LXBhcnRpY3VsaWVycy5lbmVkaXMuZnIvZ3JvdXAvZXNwYWNlLXBhcnRpY3VsaWVycy9hY2N1ZWls', // base64 of http://espace-client-particuliers.enedis.fr:80/group/espace-particuliers/accueil'
+      // gotoOnFail: '',
       SunQueryParamsString: 'cmVhbG09cGFydGljdWxpZXJz',
+      encoded: true,
+      gx_charset: 'UTF-8',
     });
 
   if (!res.ok) {
     console.error(res);
     throw new HTTPError('Error while logging in.', res.status);
   }
-  if (res.xhr.responseURL.indexOf('messages') !== -1) {
-    // Redirected to https://espace-client-connexion.enedis.fr/messages/{information,inexistant}.html
-    // which indicates a login error.
+  // Check if redirected to https://espace-client-connexion.enedis.fr/messages/{information,inexistant}.html
+  // which indicates a login error
+  const responseURL = getResponseURL(res);
+  if (responseURL.includes('Login')) {
+    // highly suspicious that we are redirected to the Login page.
+    // we should probably be redirected somewhere else
+    console.warn('loginResponseURL', responseURL);
+  } else {
+    console.warn('loginResponseURL', responseURL);
+  }
+  if (responseURL.includes('messages')) {
     throw new AuthenticationError('Invalid credentials');
   }
+  // if (res.text.includes('Votre session a expiré')) {
+  //   throw new AuthenticationError('Session expired');
+  // }
 }
 
 
@@ -69,13 +101,13 @@ function disconnect() {
 }
 
 
-async function collect(state) {
+async function collect(state, { logWarning }) {
   const { username, password } = state;
   // LogIn to set Cookies
   await logIn(username, password);
 
   // For now we're gathering daily data
-  const frequency = 'day';
+  const frequency = 'hour';
 
   // By default, go back 1 month
   // (we can't go back further using a single API call)
@@ -107,16 +139,34 @@ async function collect(state) {
     .set('If-None-Match', '*')
     .send(payload);
 
+  const responseURL = getResponseURL(res);
+  // Check if the response URL is a redirect to the login page
+  if (responseURL.includes('Login')) {
+    throw new Error('We\'re supposed to be logged in at this stage');
+  }
+
   const json = JSON.parse(res.text);
   if (json.etat.valeur === 'erreur') {
-    throw Error('Error while fetching data.');
+    throw new Error('Error while fetching data.');
+  }
+  if (json.etat.valeur === 'nonActive') {
+    throw new Error('No available data for the selected period');
   }
 
   const { data, periode } = json.graphe;
+  const offset = json.graphe.decalage || 0;
+
+  if (!Array.isArray(data)) {
+    throw new Error(`Unexpected data: ${JSON.stringify(json)}`);
+  }
 
   // TODO: Double check timezones?
   const startMoment = moment(periode.dateDebut, 'DD/MM/YYYY');
-  const endMoment = moment(periode.dateDebut, 'DD/MM/YYYY');
+  const endMoment = moment(periode.dateFin, 'DD/MM/YYYY');
+
+  // Taken from https://github.com/bokub/linky/blob/master/index.js#L156-L183
+  data.splice(0, offset);
+  data.splice(-offset, offset);
 
   /*
     Schema:
@@ -129,19 +179,30 @@ async function collect(state) {
     Negative `valeur` means data is unknown
   */
 
-  const activities = data.map((d, i) => {
-    // Remember that moment methods are mutable,
-    // which means we should make a copy by wrapping in `moment()``
-    const dateMoment = moment(startMoment).add(i, frequency);
-    return {
-      // Create a unique id to make sure data will be overwritten
-      id: `linky${dateMoment.toISOString()}`,
-      datetime: dateMoment.toDate(),
+  const parseValue = d => (d.valeur >= 0 ? d.valeur * 1000 : 0);
+
+  const activities = Object.entries(groupBy(
+    data.map((d, i) => Object.assign(d, {
+      date: moment(startMoment)
+        .add(i * STEP_GRANULARITY[frequency], frequency)
+        .startOf('day')
+        .toISOString(),
+    })),
+    d => d.date
+  ))
+    .map(([k, values]) => ({
+      id: `linky${k}`,
+      datetime: moment(k).toDate(),
       activityType: ACTIVITY_TYPE_ELECTRICITY,
-      energyWattHours: d.valeur >= 0 ? (d.valeur * 1000.0) : null,
-      durationHours: DURATION_HOURS[frequency],
-    };
-  }).filter(d => d.energyWattHours); // we currently ignore invalid hours.
+      energyWattHours: values
+        .map(parseValue) // kWh -> Wh
+        .reduce((a, b) => a + b, 0),
+      durationHours: values.length,
+      hourlyEnergyWattHours: values.map(parseValue),
+    }));
+  activities
+    .filter(d => d.durationHours !== 24)
+    .forEach(d => logWarning(`Ignoring activity from ${d.datetime.toISOString()} with ${d.durationHours} hours instead of 24`));
 
   // Subtract one day to make sure we always have a full day
   const lastFullyCollectedDay = endMoment.subtract(1, 'day').format('DD/MM/YYYY');
