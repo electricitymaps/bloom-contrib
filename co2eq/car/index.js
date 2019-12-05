abd9ff967b6ab52ef98c14068a832bba084f7d68
Ayako Ohara <ayako.ohara@icloud.com>
2019-12-05 11:03:20 -0500

    added modelCanRun()

--- co2eq/car/index.js
+++ co2eq/car/index.js
@@ -8,21 +8,31 @@ export const modelName = 'car';
 export const modelVersion = '1';
 export const explanation = {};
 
+export const modelCanRunVersion = 1;
+export function modelCanRun(activity) {
+  const {euroCarSegment, engineType, brand} = activity;
+  if (brand) {
+    return carbonIntensityByBrand(brand);
+  }
+  return carbonIntensity(euroCarSegment, engineType, brand);
+}
 
 // look up carbonIntensity by brand name
 export function carbonIntensityByBrand(brand) {
-  if (!cars[brand]) {
+  const entry = cars.footprints.find(d => d.brand === brand);
+  if (!entry) {
     throw new Error(`Unknown brand name ${brand}`);
   } else {
-    return cars[brand].carbonIntensity;
+    return entry.carbonIntensity;
   }
 }
 
-// look up carbon intensity for cars by size and type
+// look up carbon intensity for cars by Euro car segment and engine type,
+// input can be null
 export function carbonIntensity(euroCarSegment, engineType, brand) {
   const entry = cars.footprints.find(d => d.euroCarSegment === euroCarSegment && d.engineType === engineType && d.brand === brand);
   if (!entry) {
-    throw new Error(`Unknown size and type ${euroCarSegment}_${engineType}`);
+    throw new Error(`Unknown size, type, or brand ${euroCarSegment}_${engineType}_${brand}`);
   }
   return entry.carbonIntensity;
 }
