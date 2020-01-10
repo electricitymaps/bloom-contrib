3ec808ef815aaed2f107451dfec7d7b17c9d3f98
Magalii <magali.legast@student.uclouvain.be>
2020-01-10 16:45:32 +0100

    Addition of all the ISO2 country codes and the hotel footprint values

--- co2eq/hotelpercountry.js
+++ co2eq/hotelpercountry.js
@@ -26,9 +26,10 @@ The carbon intensity is per night stayed for one room
 function carbonIntensity(activity){
     //The data for all the countries can be found here: 
     //https://docs.google.com/spreadsheets/d/1f1j9EeVn9czOZBJKLXvgPwnmldakxuJ7/edit#gid=1584958883
+    //Source for the division of the "Caribbean region" in ISO2 country codes: (2020, January 9). Carribean. Retrieved from https://en.wikipedia.org/wiki/Caribbean
     if(!countries.countries[activity.countryCodeISO2.toUpperCase()].hotelFtPrint){
-        throw new Error(`Country code referenced ${countryCodeISO2} doesn't have a hotel footprint value associated to it`);
-        //Maybe add a default value here
+      //There is no data for that specific area, so the median of all the values given in the above document is returned
+      return 40.3
     }
     else{
         return countries.countries[activity.countryCodeISO2.toUpperCase()].hotelFtPrint;
