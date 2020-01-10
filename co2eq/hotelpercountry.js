6e9c7f8c894ea676a4049b44ccc0e78b01864ad3
Magalii <magali.legast@student.uclouvain.be>
2020-01-10 18:59:43 +0100

    Modification of code form to match project style requirements

--- co2eq/hotelpercountry.js
+++ co2eq/hotelpercountry.js
@@ -1,4 +1,4 @@
-import countries from "./countries.json";
+import countries from './countries.json';
 
 export const modelName = 'hotelpercountry';
 export const modelVersion = '1';
@@ -13,7 +13,7 @@ export const modelCanRunVersion = 1;
 export function modelCanRun(activity) {
   const { countryCodeISO2, durationHours } = activity;
   if (countryCodeISO2 && durationHours) {
-    if(countries.countries[activity.countryCodeISO2.toUpperCase()]){
+    if (countries.countries[activity.countryCodeISO2.toUpperCase()]) {
       return true;
     }
   }
@@ -23,22 +23,20 @@ export function modelCanRun(activity) {
 /*
 The carbon intensity is per night stayed for one room
 */
-function carbonIntensity(activity){
-    //The data for all the countries can be found here: 
-    //https://docs.google.com/spreadsheets/d/1f1j9EeVn9czOZBJKLXvgPwnmldakxuJ7/edit#gid=1584958883
-    //Source for the division of the "Caribbean region" in ISO2 country codes: (2020, January 9). Carribean. Retrieved from https://en.wikipedia.org/wiki/Caribbean
-    if(!countries.countries[activity.countryCodeISO2.toUpperCase()].hotelFtPrint){
-      //There is no data for that specific area, so the median of all the values given in the above document is returned
-      return 40.3
-    }
-    else{
-        return countries.countries[activity.countryCodeISO2.toUpperCase()].hotelFtPrint;
-    }
+function carbonIntensity(activity) {
+  // The data for all the countries can be found here: 
+  // https://docs.google.com/spreadsheets/d/1f1j9EeVn9czOZBJKLXvgPwnmldakxuJ7/edit#gid=1584958883
+  // Source for the division of the "Caribbean region" in ISO2 country codes: (2020, January 9). Carribean. Retrieved from https://en.wikipedia.org/wiki/Caribbean
+  if (!countries.countries[activity.countryCodeISO2.toUpperCase()].hotelFtPrint) {
+    // There is no data for that specific area, so the median of all the values given in the above document is returned
+    return 40.3;
+  }
+  return countries.countries[activity.countryCodeISO2.toUpperCase()].hotelFtPrint;
 }
 
 /*
 Carbon emissions of an activity (in kgCO2eq)
 */
 export function carbonEmissions(activity) {
-    return carbonIntensity(activity) * Math.ceil(activity.durationHours / 24);
-  }
+  return carbonIntensity(activity) * Math.ceil(activity.durationHours / 24);
+}
