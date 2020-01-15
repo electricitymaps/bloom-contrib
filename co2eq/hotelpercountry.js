ccf997bbfb1e93bdd49ba7a983b9be255967fca8
Magalii <magali.legast@student.uclouvain.be>
2020-01-15 09:59:59 +0100

    Change reference link to the source

--- co2eq/hotelpercountry.js
+++ co2eq/hotelpercountry.js
@@ -5,7 +5,7 @@ export const modelVersion = '1';
 export const explanation = {
   text: 'Calculations take into account the emissions of a stay depending of the country',
   links: [
-    { label: 'UK Government Conversion Factor', href: 'https://docs.google.com/spreadsheets/d/1f1j9EeVn9czOZBJKLXvgPwnmldakxuJ7/edit#gid=1584958883' },
+    { label: 'UK Government Conversion Factor', href: 'https://www.gov.uk/government/publications/greenhouse-gas-reporting-conversion-factors-2019' },
   ],
 };
 
@@ -25,7 +25,7 @@ The carbon intensity is per night stayed for one room
 */
 function carbonIntensity(activity) {
   // The data for all the countries can be found here: 
-  // https://docs.google.com/spreadsheets/d/1f1j9EeVn9czOZBJKLXvgPwnmldakxuJ7/edit#gid=1584958883
+  // https://www.gov.uk/government/publications/greenhouse-gas-reporting-conversion-factors-2019
   // Source for the division of the "Caribbean region" in ISO2 country codes: (2020, January 9). Carribean. Retrieved from https://en.wikipedia.org/wiki/Caribbean
   if (!countries.countries[activity.countryCodeISO2.toUpperCase()].hotelFtPrint) {
     // There is no data for that specific area, so the median of all the values given in the above document is returned
