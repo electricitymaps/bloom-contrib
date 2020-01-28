cf6a74027f916531c210279846a65aa6827aec45
Olivier Corradi <olivier.corradi@gmail.com>
2020-01-28 14:10:01 +0100

    WIP

--- co2eq/energy/index.js
+++ co2eq/energy/index.js
@@ -1,19 +1,7 @@
 import md5 from 'tiny-hashes/md5';
 
 import {
-  HEATING_SOURCE_COAL_BOILER,
-  HEATING_SOURCE_OIL_BOILER,
-  HEATING_SOURCE_GAS_BOILER,
-  HEATING_SOURCE_GAS_MICRO_COMBINED_HEAT_AND_POWER,
-  HEATING_SOURCE_GAS_ABSORPTION_HEAT_PUMP,
-  HEATING_SOURCE_BIOSOURCED_GASES,
-  HEATING_SOURCE_BIOMASS_BOILER,
-  HEATING_SOURCE_GEOTHERMAL,
-  HEATING_SOURCE_SOLAR_THERMAL,
-  HEATING_SOURCE_ELECTRIC_HEATER,
-  HEATING_SOURCE_GROUND_SOURCE_HEAT_PUMP,
-  HEATING_SOURCE_AIR_SOURCE_HEAT_PUMP,
-  HEATING_SOURCE_DISTRICT_HEATING,
+  ELECTRICITY_ACTIVITIES,
 } from '../../definitions';
 import energyFootprints from './energyfootprints.yml';
 
@@ -37,56 +25,51 @@ export function modelCanRun(activity) {
   return true;
 }
 
+function correctWithParticipants(footprint, participants) {
+  return footprint / (participants || 1);
+}
+
 /*
 Carbon emissions of an activity (in kgCO2eq)
 */
-export function carbonEmissions(activity) {
+function carbonEmissionsWithoutParticipants(activity) {
   const {
     countryCodeISO2,
-    durationHours,
     heatingSource,
-    costAmount,
-    costCurrency,
     energyWattHours,
   } = activity;
 
   if (!heatingSource) {
-    throw new Error(`Missing heatingSource`);
+    throw new Error('Missing heatingSource');
   }
 
   const energyFootprint = (energyFootprints[heatingSource] || {}).kWh;
   if (!energyFootprint) {
     throw new Error(`Unable to find a footprint for heatingSource ${heatingSource}`);
   }
-
-  // TODO: double check with heating purchases that things are not counted twice
-  // !!!!!!!!!!!!!!!!
-
-  if (energyWattHours) {
-    if (energyFootprint.country
-      && countryCodeISO2
-      && energyFootprint.country[countryCodeISO2]
-      && energyFootprint.country[countryCodeISO2].intensityKilograms) {
-      // TODO: kWhPerYearPerM2
-      // Get country-specific values
-      const countryCarbonIntensity = energyFootprint.country[countryCodeISO2].intensityKilograms;
-      return energyWattHours / 1000.0 * countryCarbonIntensity;
-    }
-    // Use non-country specific estimator
-    if (!energyFootprint.intensityKilograms) {
-      throw new Error(`Unable to find a non-country-specific carbon intensity for heatingSource ${heatingSource}`);
-    }
-    return energyWattHours / 1000.0 * energyFootprint.intensityKilograms;
+  if (energyWattHours == null) {
+    throw new Error('Missing energyWattHours');
   }
 
-  if (countryCodeISO2
+  // Check if we can use the country-specific estimate
+  if (energyFootprint.country
     && countryCodeISO2
     && energyFootprint.country[countryCodeISO2]
-    && energyFootprint.country[countryCodeISO2].kWhPerYear) {
-    // Try to estimate usage based on country
-    throw new Error('This isn\' implemented yet');
-  } else {
-    // Try to estimate usage without having a country
-    throw new Error('This isn\' implemented yet');
+    && energyFootprint.country[countryCodeISO2].intensityKilograms) {
+    // Get country-specific values
+    const countryCarbonIntensity = energyFootprint.country[countryCodeISO2].intensityKilograms;
+    return energyWattHours / 1000.0 * countryCarbonIntensity;
   }
+  // Use non-country specific estimator
+  if (!energyFootprint.intensityKilograms) {
+    throw new Error(`Unable to find a non-country-specific carbon intensity for heatingSource ${heatingSource}`);
+  }
+  return energyWattHours / 1000.0 * energyFootprint.intensityKilograms;
+}
+
+export function carbonEmissions(activity) {
+  return correctWithParticipants(
+    carbonEmissionsWithoutParticipants(activity),
+    activity.participants
+  );
 }
