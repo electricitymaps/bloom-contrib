8f4f6039ad40dde86f22641c0a3bab60726a4ac6
Mads Nedergaard <nedergaardmads@gmail.com>
2020-02-26 14:36:33 +0100

    Removes "contrib" that slows down (+ rm core-js)

--- playground/webpack.config.js
+++ playground/webpack.config.js
@@ -19,12 +19,6 @@ module.exports = {
             presets: [
               [
                 '@babel/preset-env',
-                {
-                  modules: 'false', // commonjs,amd,umd,systemjs,auto
-                  useBuiltIns: 'usage',
-                  targets: '> 0.25%, not dead',
-                  corejs: 3,
-                },
               ],
             ],
           },
