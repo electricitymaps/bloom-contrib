71416338685d6df173613baa4817c049ed591786
David Friederich <dav.friede@gmail.com>
2020-03-19 11:04:14 +0100

    uses moment for safely parsing datetimes in utils

--- co2eq/utils.js
+++ co2eq/utils.js
@@ -1,9 +1,11 @@
+import moment from "moment";
+
 /**
  * Returns the duration in hours between the start and end of an activity
  * @param {Activity} activity Activity
  * @returns {number} Duration in hours between start and end of an activity. Returns null if no endDatetime
  */
-export function getActivityDurationHours(activity) {
+export function getActivityDurationHours(activity, precise = true) {
     if (!activity.endDatetime) return null;
-    return (activity.endDatetime - activity.datetime) / 36e5;
+    return moment(activity.endDatetime).diff(moment(activity.datetime), 'hours', precise);
 }
