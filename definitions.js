39cd9875e46f68ba5eaf6bb51d4d0496f7508e06
Olivier Corradi <1655848+corradio@users.noreply.github.com>
2020-04-06 15:43:24 +0200

    Add currency conversion test (#379)
    * Add currency conversion test
    
    * Updates currency definition + test
    
    Co-authored-by: Mads Nedergaard <nedergaardmads@gmail.com>

--- definitions.js
+++ definitions.js
@@ -22,6 +22,42 @@ export const UNITS = [
   UNIT_CUP,
 ];
 
+export const UNIT_CURRENCIES = {
+  AUD: 'AUD',
+  BGN: 'BGN',
+  BRL: 'BRL',
+  CAD: 'CAD',
+  CHF: 'CHF',
+  CNY: 'CNY',
+  CZK: 'CZK',
+  DKK: 'DKK',
+  EUR: 'EUR',
+  GBP: 'GBP',
+  HKD: 'HKD',
+  HRK: 'HRK',
+  HUF: 'HUF',
+  IDR: 'IDR',
+  ILS: 'ILS',
+  INR: 'INR',
+  ISK: 'ISK',
+  JPY: 'JPY',
+  KRW: 'KRW',
+  MXN: 'MXN',
+  MYR: 'MYR',
+  NOK: 'NOK',
+  NZD: 'NZD',
+  PHP: 'PHP',
+  PLN: 'PLN',
+  RON: 'RON',
+  RUB: 'RUB',
+  SEK: 'SEK',
+  SGD: 'SGD',
+  THB: 'THB',
+  TRY: 'TRY',
+  USD: 'USD',
+  ZAR: 'ZAR',
+};
+
 /* Activity Types
 Each activity type is tied to a specific UI
 This is why it's important to group the electricity activities together
