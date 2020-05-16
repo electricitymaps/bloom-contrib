2f3bb933ca7dc81abd8d37c0ea76b14c5f6cf3a7
Vincent Biret <vincentbiret@hotmail.com>
2020-05-16 15:27:01 -0400

    - lints

--- co2eq/electricity/worldaverage.js
+++ co2eq/electricity/worldaverage.js
@@ -1,5 +1,5 @@
-import { ELECTRICITY_ACTIVITIES } from '../../definitions';
 import { featuresContaining } from '@ideditor/country-coder';
+import { ELECTRICITY_ACTIVITIES } from '../../definitions';
 import { factors } from './emissionfactors.json';
 
 // ** modelName must not be changed. If changed then old activities will not be re-calculated **
@@ -36,25 +36,27 @@ function correctWithParticipants(footprint, participants) {
 
 function getCarbonFactor(locationLon, locationLat) {
   const features = featuresContaining([locationLon, locationLat]);
-  if(!features || features.length === 0) {
+  if (!features || features.length === 0) {
     return undefined;
   }
   const countries = features.filter(x => x.properties.level === 'country');
-  if(countries.length === 0) {
+  if (countries.length === 0) {
     return undefined;
   }
   const country = countries[0];
   const potentialStates = features.filter(x => x.level === 'territory');
   const state = potentialStates && potentialStates.length > 0 ? potentialStates[0] : undefined;
   const potentialFactors = factors.filter(x => x.country === country.properties.iso1A3);
-  if(potentialFactors.length === 0) {
+  if (potentialFactors.length === 0) {
     return undefined;
-  } else if(potentialFactors.length === 1 || !state) {
-    return potentialFactors[0].factor;
-  } else {
-    const potentialStateFactors = potentialFactors.filter(x => x.state === state.properties.iso1A2);
-    return potentialStateFactors.length === 0 ? potentialFactors[0].factor : potentialStateFactors[0].factor;
   }
+  if (potentialFactors.length === 1 || !state) {
+    return potentialFactors[0].factor;
+  }
+  const potentialStateFactors = potentialFactors.filter(x => x.state === state.properties.iso1A2);
+  return potentialStateFactors.length === 0
+    ? potentialFactors[0].factor
+    : potentialStateFactors[0].factor;
 }
 
 /*
@@ -64,11 +66,11 @@ function carbonEmissionsWithoutParticipants(activity) {
   let carbonFactor = worldCarbonIntensity;
   if (activity.locationLon && activity.locationLat) {
     const preciseFactor = getCarbonFactor(activity.locationLon, activity.locationLat);
-    if(preciseFactor) {
+    if (preciseFactor) {
       carbonFactor = preciseFactor;
     }
   }
-  return ((activity.energyWattHours / 1000.0) * carbonFactor ) / 1000.0;
+  return ((activity.energyWattHours / 1000.0) * carbonFactor) / 1000.0;
 }
 
 export function carbonEmissions(activity) {
