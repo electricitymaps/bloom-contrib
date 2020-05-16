aee9d62d390de67bed045daa0ede3f7970514d4c
Vincent Biret <vincentbiret@hotmail.com>
2020-05-16 14:48:37 -0400

    - adds support for localized electric emission factors

---
+++ co2eq/electricity/emissionfactors.test.js
@@ -0,0 +1,27 @@
+import {
+    ACTIVITY_TYPE_ELECTRICITY,
+} from '../../definitions';
+
+import { carbonEmissions } from './worldaverage';
+
+describe('model uses emission factor', () => {
+    const wattHours = 1000;
+    test(`with default`, () => {
+        const activity = {
+            activityType: ACTIVITY_TYPE_ELECTRICITY,
+            energyWattHours: wattHours,
+        };
+        
+        expect(carbonEmissions(activity)).toBe(0.475);
+    });
+    test(`with Montreals factor`, () => {
+        const activity = {
+            activityType: ACTIVITY_TYPE_ELECTRICITY,
+            energyWattHours: wattHours,
+            locationLon: -73.55468,
+            locationLat: 45.5124,
+        };
+        
+        expect(carbonEmissions(activity)).toBe(0.0015);
+    });
+});
--- co2eq/electricity/worldaverage.js
+++ co2eq/electricity/worldaverage.js
@@ -1,4 +1,6 @@
 import { ELECTRICITY_ACTIVITIES } from '../../definitions';
+import { featuresContaining } from '@ideditor/country-coder';
+import { factors } from './emissionfactors.json';
 
 // ** modelName must not be changed. If changed then old activities will not be re-calculated **
 export const modelName = 'electricity-world-average';
@@ -11,12 +13,17 @@ export const explanation = {
       label: 'IEA (2019)',
       href: 'https://www.iea.org/reports/global-energy-co2-status-report-2019/emissions',
     },
+    {
+      label: 'Carbon footprint',
+      href:
+        'https://www.carbonfootprint.com/docs/2019_06_emissions_factors_sources_for_2019_electricity.pdf',
+    },
   ],
 };
 
 const worldCarbonIntensity = 475; // g/kWh
 
-export const modelCanRunVersion = 1;
+export const modelCanRunVersion = 2;
 export function modelCanRun(activity) {
   const { energyWattHours, activityType } = activity;
   const isElectricActivity = ELECTRICITY_ACTIVITIES.includes(activityType);
@@ -27,11 +34,41 @@ function correctWithParticipants(footprint, participants) {
   return footprint / (participants || 1);
 }
 
+function getCarbonFactor(locationLon, locationLat) {
+  const features = featuresContaining([locationLon, locationLat]);
+  if(!features || features.length === 0) {
+    return undefined;
+  }
+  const countries = features.filter(x => x.properties.level === 'country');
+  if(countries.length === 0) {
+    return undefined;
+  }
+  const country = countries[0];
+  const potentialStates = features.filter(x => x.level === 'territory');
+  const state = potentialStates && potentialStates.length > 0 ? potentialStates[0] : undefined;
+  const potentialFactors = factors.filter(x => x.country === country.properties.iso1A3);
+  if(potentialFactors.length === 0) {
+    return undefined;
+  } else if(potentialFactors.length === 1 || !state) {
+    return potentialFactors[0].factor;
+  } else {
+    const potentialStateFactors = potentialFactors.filter(x => x.state === state.properties.iso1A2);
+    return potentialStateFactors.length === 0 ? potentialFactors[0].factor : potentialStateFactors[0].factor;
+  }
+}
+
 /*
 Carbon emissions of an activity (in kgCO2eq)
 */
 function carbonEmissionsWithoutParticipants(activity) {
-  return ((activity.energyWattHours / 1000.0) * worldCarbonIntensity) / 1000.0;
+  let carbonFactor = worldCarbonIntensity;
+  if (activity.locationLon && activity.locationLat) {
+    const preciseFactor = getCarbonFactor(activity.locationLon, activity.locationLat);
+    if(preciseFactor) {
+      carbonFactor = preciseFactor;
+    }
+  }
+  return ((activity.energyWattHours / 1000.0) * carbonFactor ) / 1000.0;
 }
 
 export function carbonEmissions(activity) {
