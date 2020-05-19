ac85daeb5804651ebff41a8ddbde1aa060d05414
Vincent Biret <vincentbiret@hotmail.com>
2020-05-19 11:27:33 -0400

    - adds support for localized electric emission factors (#399)
    * - adds support for localized electric emission factors
    
    * - updates yarn lock
    
    * - lints
    
    * - adds state factor support for CA and US
    
    * - adds australia emission factors
    
    * - adds usa states emission factors
    
    * - adds unit tests for USA and AUS factors
    
    * - adds europe emission factors
    
    * - fixes londons unit test
    
    * - adds remaining emission factors
    
    * - updates parameters to follow conventions
    
    * - aligns format of factors
    
    * - replaces string empty to null for state
    
    * - adds years for emission factors

---
+++ co2eq/electricity/emissionfactors.test.js
@@ -0,0 +1,64 @@
+import { ACTIVITY_TYPE_ELECTRICITY } from '../../definitions';
+import { carbonEmissions } from './worldaverage';
+
+describe('model uses emission factor', () => {
+  const wattHours = 1000;
+  test(`with default`, () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_ELECTRICITY,
+      energyWattHours: wattHours,
+    };
+
+    expect(carbonEmissions(activity)).toBe(0.475);
+  });
+  test(`with Montreals factor`, () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_ELECTRICITY,
+      energyWattHours: wattHours,
+      locationLat: 45.5124,
+      locationLon: -73.55468,
+    };
+
+    expect(carbonEmissions(activity)).toBe(0.0015);
+  });
+  test(`with Torontos factor`, () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_ELECTRICITY,
+      energyWattHours: wattHours,
+      locationLat: 43.761539,
+      locationLon: -79.411079,
+    };
+
+    expect(carbonEmissions(activity)).toBe(0.02);
+  });
+  test(`with New Yorks factor`, () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_ELECTRICITY,
+      energyWattHours: wattHours,
+      locationLat: 40.73061,
+      locationLon: -73.935242,
+    };
+
+    expect(carbonEmissions(activity)).toBeCloseTo(0.2212); // roundup issue here
+  });
+  test(`with Sidneys factor`, () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_ELECTRICITY,
+      energyWattHours: wattHours,
+      locationLat: -33.865143,
+      locationLon: 151.2099,
+    };
+
+    expect(carbonEmissions(activity)).toBe(0.92);
+  });
+  test(`with Londons factor`, () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_ELECTRICITY,
+      energyWattHours: wattHours,
+      locationLat: 51.509865,
+      locationLon: -0.118092,
+    };
+
+    expect(carbonEmissions(activity)).toBe(0.2773);
+  });
+});
