90926fdb9f3223c2b8d23422c4031e92974b0dc7
martincollignon <2604526+martincollignon@users.noreply.github.com>
2020-06-09 22:44:57 +0200

    Update index.test.js

--- co2eq/purchase/index.test.js
+++ co2eq/purchase/index.test.js
@@ -6,7 +6,10 @@ import {
   ACTIVITY_TYPE_PURCHASE,
 } from '../../definitions';
 import { getDescendants, getRootEntry, modelCanRun, carbonEmissions } from './index';
-import { getAvailableCurrencies, getAvailableCurrencies2011 } from '../../integrations/utils/currency/currency';
+import { 
+  getAvailableCurrencies,
+  getAvailableCurrencies2011 
+} from '../../integrations/utils/currency/currency';
 
 Object.entries(getDescendants(getRootEntry()))
   .filter(([k, v]) => v.unit)
