04c3c977373fbdf6742329809da8cc9d3abd88c6
martincollignon <2604526+martincollignon@users.noreply.github.com>
2020-06-09 22:19:05 +0200

    Added 2011 exchange rates to currency.js

--- integrations/utils/currency/currency.js
+++ integrations/utils/currency/currency.js
@@ -1,4 +1,5 @@
 const exchangeRates = require('./exchange_rates.json');
+const exchangeRates2011 = require('./exchange_rates_2011.json');
 
 export function convertToEuro(amount, currency) {
   if (currency.toUpperCase() === 'EUR') return amount;
@@ -8,6 +9,17 @@ export function convertToEuro(amount, currency) {
   return amount / exchangeRate;
 }
 
+export function convertTo2011Euro(amount, currency) {
+  if (currency.toUpperCase() === 'EUR') return amount;
+
+  const exchangeRate2011 = exchangeRates2011.rates[currency.toUpperCase()];
+  if (!exchangeRate2011) throw new Error(`Unknown currency '${currency}'`);
+  return amount / exchangeRate2011;
+}
+
 export function getAvailableCurrencies() {
   return [...Object.keys(exchangeRates.rates), 'EUR'];
+}
+export function getAvailableCurrencies2011() {
+  return [...Object.keys(exchangeRates2011.rates), 'EUR'];
 }
