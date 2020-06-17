a693d113ad4362bafb78592e8d33697083344b3d
martincollignon <2604526+martincollignon@users.noreply.github.com>
2020-06-17 13:31:28 +0200

    Update index.js

--- co2eq/purchase/index.js
+++ co2eq/purchase/index.js
@@ -18,6 +18,23 @@ import { getChecksum } from '../utils';
 import footprints from './footprints.yml';
 import consumerPriceIndex from './consumerpriceindices.yml';
 
+const exchangeRates2011 = require('./exchange_rates_2011.json');
+
+export function convertTo2011Euro(amount, currency) {
+  if (currency.toUpperCase() === 'EUR') return amount;
+
+  const exchangeRate2011 = exchangeRates2011.rates[currency.toUpperCase()];
+  if (!exchangeRate2011) throw new Error(`Unknown currency '${currency}'`);
+  return amount / exchangeRate2011;
+}
+
+export function getAvailableCurrencies2011() {
+  return [...Object.keys(exchangeRates2011.rates), 'EUR'];
+}
+
+
+
+
 const AVERAGE_CPI_COUNTRY_INDICATOR = 'average';
 const COUNTRY_CPI_INDICATOR = 'countries';
 
