7fabc471022ae4bb32c9df7a30db6d10656bbb9f
martincollignon <2604526+martincollignon@users.noreply.github.com>
2020-06-17 13:26:45 +0200

    Update currency.js

--- integrations/utils/currency/currency.js
+++ integrations/utils/currency/currency.js
@@ -1,5 +1,4 @@
 const exchangeRates = require('./exchange_rates.json');
-const exchangeRates2011 = require('./exchange_rates_2011.json');
 
 export function convertToEuro(amount, currency) {
   if (currency.toUpperCase() === 'EUR') return amount;
@@ -9,17 +8,6 @@ export function convertToEuro(amount, currency) {
   return amount / exchangeRate;
 }
 
-export function convertTo2011Euro(amount, currency) {
-  if (currency.toUpperCase() === 'EUR') return amount;
-
-  const exchangeRate2011 = exchangeRates2011.rates[currency.toUpperCase()];
-  if (!exchangeRate2011) throw new Error(`Unknown currency '${currency}'`);
-  return amount / exchangeRate2011;
-}
-
 export function getAvailableCurrencies() {
   return [...Object.keys(exchangeRates.rates), 'EUR'];
-}
-export function getAvailableCurrencies2011() {
-  return [...Object.keys(exchangeRates2011.rates), 'EUR'];
 }
