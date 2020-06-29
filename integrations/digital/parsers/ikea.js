14e12f2d938b3a40f294e16f278b02481915339f
Vincent Biret <vincentbiret@hotmail.com>
2020-06-29 07:33:24 -0400

    adds email model, adds outlook as data source (#401)
    * - adds required dependencies
    
    * - adds email carbon model
    
    * - fixes an issue where scope would not be passed properly
    
    * - adds outlook email inbox as a data source
    
    * - lints
    
    * - adds support for multiple folders
    
    * - adds ikea parser
    
    * - adds parers integration into outlook
    
    * - adds documentation on how to add an email parser
    
    * - adds a dedicated getEmailActivities to simplify running parsers
    
    * - disables digital footprint activities import
    
    * - adds amazon email parser
    
    * Apply suggestions from code review
    
    Co-authored-by: Olivier Corradi <1655848+corradio@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * - removes model following pr review
    
    * - fixes linting issue
    
    * - removes unecessary dependency
    - minor pr adjustements
    
    Co-authored-by: Olivier Corradi <1655848+corradio@users.noreply.github.com>

---
+++ integrations/digital/parsers/ikea.js
@@ -0,0 +1,38 @@
+import {
+  ACTIVITY_TYPE_PURCHASE,
+  UNIT_CURRENCIES,
+  PURCHASE_CATEGORY_STORE_FURNISHING,
+} from '../../../definitions';
+
+const fromEmail = 'donotreply@ikea.com';
+function getCurrencyAndCountryCode(bodyAsHtml) {
+  if (bodyAsHtml.indexOf('IKEA Canada') > -1) {
+    return { cur: UNIT_CURRENCIES.CAD, code: 'CA' };
+  }
+  return { cur: UNIT_CURRENCIES.EUR, code: 'FR' };
+}
+export function evaluateEmail(subject, from, bodyAsHtml, sendDate) {
+  if (from !== undefined && from.toLowerCase() === fromEmail) {
+    const priceMatches = /<b>\$([\d,.]+)/gm.exec(bodyAsHtml);
+    const orderMatches = /(\d{9})/gm.exec(bodyAsHtml);
+    const currencyAndCode = getCurrencyAndCountryCode(bodyAsHtml);
+    if (priceMatches && priceMatches.length > 1 && priceMatches[1]) {
+      return {
+        id: `IKEA-${orderMatches.length > 1 && orderMatches[1]}`,
+        datetime: sendDate,
+        label: `Ikea order ${orderMatches.length > 1 && orderMatches[1]}`,
+        activityType: ACTIVITY_TYPE_PURCHASE,
+        supplierName: 'Ikea',
+        lineItems: [
+          {
+            id: PURCHASE_CATEGORY_STORE_FURNISHING,
+            value: parseFloat(priceMatches[1]),
+            countryCodeISO2: currencyAndCode.code,
+            unit: currencyAndCode.cur,
+          },
+        ],
+      };
+    }
+  }
+  return undefined;
+}
