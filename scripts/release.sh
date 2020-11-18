67d9fa7365c89131843de85039e124b73e258766
skovhus <kenneth.skovhus@gmail.com>
2020-11-18 23:04:01 +0100

    Add auto releasing of package to npm
    Until the package is stable I see no need for manually bumping version numbers.
    Once we have a stable package, then we can follow semantic versioning.

---
+++ scripts/release.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Publish from dist folder to get cleaner imports
+cp package.json LICENSE.txt README.md dist/
+cd dist
+
+if [ -z "$NPM_TOKEN" ]; then
+  echo "Expected NPM_TOKEN to be defined"
+  exit 1
+fi
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
+
+# Bump the patch version number
+npm version patch
+
+echo "Deploying"
+npm publish --access public
+
+# Push new version commit and tags
+git push origin --tags
