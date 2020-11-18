62aef58d3f2c7cce9fad0fa5207e02083b7f5df3
skovhus <kenneth.skovhus@gmail.com>
2020-11-18 23:54:19 +0100

    Include JSON files in package

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,3 +9,6 @@ rm -rf ${DIST_FOLDER}
 yarn --silent babel co2eq          ${BABEL_CONFIG} --out-dir ${DIST_FOLDER}/co2eq
 yarn --silent babel integrations   ${BABEL_CONFIG} --out-dir ${DIST_FOLDER}/integrations
 yarn --silent babel definitions.js ${BABEL_CONFIG} --out-dir ${DIST_FOLDER}
+
+yarn --silent copyfiles co2eq/**/*.json ${DIST_FOLDER}
+yarn --silent copyfiles integrations/**/*.json ${DIST_FOLDER}
