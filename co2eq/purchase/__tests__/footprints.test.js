40a5103e05c447d1dabd247a845a1cecc2c51d56
Pierre Segonne <pierre.segonne@student.ecp.fr>
2020-11-18 11:24:41 +0100

    Dump for checking out.

---
+++ co2eq/purchase/__tests__/footprints.test.js
@@ -0,0 +1,6 @@
+/**
+ * Check
+ * 1. activityType named used exist in constant, same for transportationModes
+ * 2. parent child relationship for activityTypes
+ * 3. categories have correct names in definitions or vice versa
+ */
