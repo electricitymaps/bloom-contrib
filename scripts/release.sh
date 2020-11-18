b0329caab921184d1bc0ce94b42d1cef38e8d6b7
skovhus <kenneth.skovhus@gmail.com>
2020-11-18 23:59:43 +0100

    Remove NPM_TOKEN check (covered by unbound flags)

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,6 @@
 
 set -euo pipefail
 
-if [ -z "$NPM_TOKEN" ]; then
-  echo "Expected NPM_TOKEN to be defined"
-  exit 1
-fi
-
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
 # Bump the patch version number
