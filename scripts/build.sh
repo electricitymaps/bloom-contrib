4b132bbbd4dcd78af08949308c5b97057a99af2b
skovhus <kenneth.skovhus@gmail.com>
2020-11-19 11:02:13 +0100

    Prevent that glob leaves out root files

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,5 +10,5 @@ yarn --silent babel co2eq          ${BABEL_CONFIG} --out-dir ${DIST_FOLDER}/co2e
 yarn --silent babel integrations   ${BABEL_CONFIG} --out-dir ${DIST_FOLDER}/integrations
 yarn --silent babel definitions.js ${BABEL_CONFIG} --out-dir ${DIST_FOLDER}
 
-yarn --silent copyfiles co2eq/**/*.json ${DIST_FOLDER}
-yarn --silent copyfiles integrations/**/*.json ${DIST_FOLDER}
+yarn --silent copyfiles "co2eq/**/*.json" ${DIST_FOLDER}
+yarn --silent copyfiles "integrations/**/*.json" ${DIST_FOLDER}
