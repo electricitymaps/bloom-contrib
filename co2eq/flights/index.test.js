81c1d10d9a44b037b032114ccf96cf37181eea78
Mads Nedergaard <nedergaardmads@gmail.com>
2020-11-29 22:52:09 +0100

    Adds tests for uncovered code

--- co2eq/flights/index.test.js
+++ co2eq/flights/index.test.js
@@ -1,28 +1,52 @@
 import { ACTIVITY_TYPE_TRANSPORTATION, TRANSPORTATION_MODE_PLANE } from '../../definitions';
 import { carbonEmissions, modelCanRun } from './index';
 
-test(`test flight with airport codes`, () => {
-  const activity = {
-    activityType: ACTIVITY_TYPE_TRANSPORTATION,
-    transportationMode: TRANSPORTATION_MODE_PLANE,
-    datetime: new Date('2020-04-11T10:20:30Z'),
-    departureAirportCode: 'ATH',
-    destinationAirportCode: 'CPH',
-  };
-  expect(modelCanRun(activity)).toBeTruthy();
-  expect(carbonEmissions(activity)).toBeCloseTo(483.07621051458455);
-});
+describe('model runs', () => {
+  it('calculates based on airport codes', () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_TRANSPORTATION,
+      transportationMode: TRANSPORTATION_MODE_PLANE,
+      datetime: new Date('2020-04-11T10:20:30Z'),
+      departureAirportCode: 'ATH',
+      destinationAirportCode: 'CPH',
+    };
 
-test(`test flight with airport codes 2`, () => {
-  // ERROR: footprint is negative
-  const activity = {
-    activityType: ACTIVITY_TYPE_TRANSPORTATION,
-    transportationMode: TRANSPORTATION_MODE_PLANE,
-    datetime: new Date('2020-04-11T10:20:30Z'),
-    endDatetime: new Date('2020-04-11T17:20:30Z'),
-    departureAirportCode: 'JFK',
-    destinationAirportCode: 'LHR',
-  };
-  expect(modelCanRun(activity)).toBeTruthy();
-  expect(carbonEmissions(activity)).toBeCloseTo(864.1030116308044);
+    expect(modelCanRun(activity)).toBeTruthy();
+    expect(carbonEmissions(activity)).toBeCloseTo(483.07621051458455);
+  });
+  it('throws on non existing airport code', () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_TRANSPORTATION,
+      transportationMode: TRANSPORTATION_MODE_PLANE,
+      datetime: new Date('2020-04-11T10:20:30Z'),
+      departureAirportCode: 'ATH',
+      destinationAirportCode: 'not-real',
+    };
+    expect(modelCanRun(activity)).toBeTruthy();
+
+    expect(() => carbonEmissions(activity)).toThrowError('Unknown airport code not-real');
+  });
+
+  it('calculates based on distance in kilometers', () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_TRANSPORTATION,
+      transportationMode: TRANSPORTATION_MODE_PLANE,
+      distanceKilometers: 2000,
+    };
+
+    expect(modelCanRun(activity)).toBeTruthy();
+    expect(carbonEmissions(activity)).toBeCloseTo(345.9374385616095);
+  });
+
+  it('calculates based on duration', () => {
+    const activity = {
+      activityType: ACTIVITY_TYPE_TRANSPORTATION,
+      transportationMode: TRANSPORTATION_MODE_PLANE,
+      datetime: new Date('2020-04-11T10:20:30Z'),
+      endDatetime: new Date('2020-04-11T12:20:30Z'),
+    };
+
+    expect(modelCanRun(activity)).toBeTruthy();
+    expect(carbonEmissions(activity)).toBeCloseTo(236.74911026391652);
+  });
 });
