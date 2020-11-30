d1316072b05d6e4b21627bca4f803494a419f289
Mads Nedergaard <nedergaardmads@gmail.com>
2020-11-30 11:18:41 +0100

    Updates flight model to calculate proper emissions

--- co2eq/flights/index.test.js
+++ co2eq/flights/index.test.js
@@ -49,17 +49,66 @@ describe('model runs', () => {
     expect(modelCanRun(activity)).toBeTruthy();
     expect(carbonEmissions(activity)).toBeCloseTo(236.74911026391652);
   });
+  describe('roundtrips', () => {
+    it('doubles emissions given distance in kilometers', () => {
+      const activity = {
+        activityType: ACTIVITY_TYPE_TRANSPORTATION,
+        transportationMode: TRANSPORTATION_MODE_PLANE,
+        distanceKilometers: 2000,
+        isRoundtrip: true,
+      };
 
-  it('supports roundtrips', () => {
-    const activity = {
-      activityType: ACTIVITY_TYPE_TRANSPORTATION,
-      transportationMode: TRANSPORTATION_MODE_PLANE,
-      datetime: new Date('2020-04-11T10:20:30Z'),
-      endDatetime: new Date('2020-04-11T12:20:30Z'),
-      isRoundtrip: true,
-    };
+      expect(modelCanRun(activity)).toBeTruthy();
+      expect(carbonEmissions(activity)).toBeCloseTo(345.9374385616095 * 2);
+    });
 
-    expect(modelCanRun(activity)).toBeTruthy();
-    expect(carbonEmissions(activity)).toBeCloseTo(236.749 * 2);
+    it('doubles emissions given duration', () => {
+      const activity = {
+        activityType: ACTIVITY_TYPE_TRANSPORTATION,
+        transportationMode: TRANSPORTATION_MODE_PLANE,
+        datetime: new Date('2020-04-11T10:20:30Z'),
+        endDatetime: new Date('2020-04-11T12:20:30Z'),
+        isRoundtrip: true,
+      };
+
+      expect(modelCanRun(activity)).toBeTruthy();
+      expect(carbonEmissions(activity)).toBeCloseTo(236.74911026391652 * 2);
+    });
+
+    it('calculates correct emissions given airport codes', () => {
+      // Due to loadfactors, etc. there is no guarantee that a return trip is the same emission as
+      // one-waytrip * 2. Therefore we calculate the emissions of the two individual flights to
+      // compare the numbers.
+      const activityWithReturn = {
+        activityType: ACTIVITY_TYPE_TRANSPORTATION,
+        transportationMode: TRANSPORTATION_MODE_PLANE,
+        datetime: new Date('2020-04-11T10:20:30Z'),
+        endDatetime: new Date('2020-04-11T12:20:30Z'),
+        departureAirportCode: 'IRA',
+        destinationAirportCode: 'CPH',
+        isRoundtrip: true,
+      };
+      const activityOut = {
+        activityType: ACTIVITY_TYPE_TRANSPORTATION,
+        transportationMode: TRANSPORTATION_MODE_PLANE,
+        datetime: new Date('2020-04-11T10:20:30Z'),
+        endDatetime: new Date('2020-04-11T12:20:30Z'),
+        departureAirportCode: 'IRA',
+        destinationAirportCode: 'CPH',
+      };
+      const activityBack = {
+        activityType: ACTIVITY_TYPE_TRANSPORTATION,
+        transportationMode: TRANSPORTATION_MODE_PLANE,
+        datetime: new Date('2020-04-11T10:20:30Z'),
+        endDatetime: new Date('2020-04-11T12:20:30Z'),
+        departureAirportCode: 'CPH',
+        destinationAirportCode: 'IRA',
+      };
+
+      const combinedEmissions = carbonEmissions(activityOut) + carbonEmissions(activityBack);
+      const returnEmissions = carbonEmissions(activityWithReturn);
+
+      expect(returnEmissions).toEqual(combinedEmissions);
+    });
   });
 });
